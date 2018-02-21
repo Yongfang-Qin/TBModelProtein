437da0afbfb368763b0957be54f303003ad82360
Yongfang Qin <yongfangqin@mwc-034031.dhcp.missouri.edu>
2018-02-21 10:40:51 -0600

    add

---
+++ Coordinate.py
@@ -0,0 +1,23 @@
+import numpy as np
+from Bio.Blast import NCBIWWW
+
+
+
+for line in open('TemplateBest.pdb'):
+    list = line.split()
+    id = list[0]
+    if id == 'ATOM':
+        type = list[2]
+        if type == 'CA':
+            id = list[1]
+            residue = list[3]
+            type_of_chain = list[4]
+            atom_count = int(list[5])
+            position = list[6:9]
+            print(id)
+            print(position)
+
+
+
+
+
