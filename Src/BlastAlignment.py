b88c6113fd0b110572a31b3536e705b108f5825f
Yongfang Qin <yongfangqin@mwc-034031.dhcp.missouri.edu>
2018-02-25 14:40:06 -0600

    add

--- Src/BlastAlignment.py
+++ Src/BlastAlignment.py
@@ -24,25 +24,49 @@ for blast_record in blast_records:
 
             identities=hsp.identities
             similarity=(100 * hsp.identities / seq_len)
+            print(similarity)
             target=hsp.query
             targetstart=hsp.query_start
             templatestart = hsp.sbjct_start
             match=hsp.match
-            template=hsp.sbjct[0:75]
+            template=hsp.sbjct[0:137]
+            print(template)
+            print(len(template))
             templatestart=hsp.sbjct_start
 
-            if similarity!=100:
+            if similarity!=100 and similarity > 70:
 
                 with open('../Data/T0860/T0860_target.pdb', 'r') as targetFile, \
                         open('../Data/T0860/5fld.pdb', 'r') as templateFile, \
-                        open('../Data/T0860/results.pdb', 'w') as outputFile, \
+                        open('../Data/T0860/results.pdb','r') as outputFile, \
                         open('../Data/T0860/results_init.pdb', 'w') as output_initFile:
 
-                    for template_line in templateFile:
-                        temp_output = template_line.split()
-                        id = temp_output[0]
-                        if id == 'ATOM':
-                            outputFile.write('\t'.join(temp_output) + '\n')
+                    #for template_line in templateFile:
+                    #   temp_output = template_line.split()
+                    #   id = temp_output[0]
+                    #  if id == 'ATOM':
+                    #     outputFile.write('\t'.join(temp_output) + '\n')
+
+                    i = 0
+                    for template_line,line in zip(outputFile,targetFile):
+                        out = template_line.split()
+                        target = line.split()
+                        i = int(target[5])
+                        if template[i] != '-':
+                            target[6]= out[6]
+                            target[7]= out[7]
+                            target[8] = out[8]
+                            target[9] = out[9]
+                            target[10] = out[10]
+                            output_initFile.write('\t'.join(target) + '\n')
+                        else:
+                            target[6] = '0'
+                            target[7] = '0'
+                            target[8] = '0'
+                            target[9] = '0'
+                            target[10] = '0'
+                            output_initFile.write('\t'.join(target) + '\n')
+
 
 
 
