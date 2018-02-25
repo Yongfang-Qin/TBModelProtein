78404fea42066e07daf7cf22f6b6d368ca16dba9
markvassell <mdvy96@mail.missorui.edu>
2018-02-24 22:01:15 -0600

    Slight update on getting the pdb file. View eidit_pdb_test.py and results.pdb

--- Src/edit_pdb_test.py
+++ Src/edit_pdb_test.py
@@ -1,10 +1,38 @@
-with open('../Data/T0900_Target.pdb', 'r') as inputFile, open('../Data/updated_file.pdb', 'w') as outputFile:
-    for line in inputFile:
-        atom = line.split()
+with open('../Data/1a40.pdb', 'r') as targetFile, open('../Data/1ixi.pdb', 'r') as templateFile, open('../Data/results.pdb', 'w') as outFile:
+    # for line in inputFile:
+    #     atom = line.split()
+    #
+    #     if atom[2] == 'CA':
+    #         atom[6],atom[7],atom[8], atom[9], atom[10] =  '   \t33.00', '44.00', '55.00', '101','23.0'
+    #         outputFile.write('\t'.join(atom)+'\n')
+    #     else:
+    #         outputFile.write(line)
 
-        if atom[2] == 'CA':
-            atom[6],atom[7],atom[8], atom[9], atom[10] =  '   \t33.00', '44.00', '55.00', '101','23.0'
-            outputFile.write('\t'.join(atom)+'\n')
-        else:
-            outputFile.write(line)
 
+    target_data = list()
+    template_data = list()
+    total_lenght = 0
+    # getting data from target file
+    for line in targetFile:
+        out_data = line.split()
+        if len(out_data) > 3 and out_data[0] == 'ATOM' and out_data[2] == 'CA':
+            target_data.append(out_data)
+            total_lenght += 1
+
+
+    print(total_lenght)
+
+    #getting data in template file
+    for line in templateFile:
+        out_data = line.split()
+        if len(out_data) > 3 and out_data[0] == 'ATOM' and out_data[2] == 'CA':
+            template_data.append(out_data)
+    current_length = 0
+    for line1, line2 in zip(target_data, template_data):
+        if current_length < total_lenght:
+            if line1[3] == line2[3]:
+                outFile.write('\t'.join(line1)+'\n')
+            else:
+                outFile.write('Data dosen\'t match\n')
+        current_length += 1
+#print(''.join(same))
